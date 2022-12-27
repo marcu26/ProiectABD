850aae2d683b35c5856dc42ab19db46a0f36dc16
Toma Marcu <toma.ovidiu@isoft-development.ro>
2022-12-27 16:25:44 +0200

    Created DB entries

--- Core/Database/Context/ProjectDbContext.cs
+++ Core/Database/Context/ProjectDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Core.Database.Entities;
 using System.Data.Entity;
-
+using System.Security.Permissions;
 
 namespace Core.Database.Context
 {
@@ -9,11 +9,28 @@ namespace Core.Database.Context
         public ProjectDbContext() : base("name=ProjectABD") { }
 
         public DbSet<Author> Authors {get; set;}
-
-      
+        public DbSet<Article> Articles {get; set;}
+        public DbSet<Book> Books {get; set;}
+        public DbSet<Journal> Journals {get; set;}
+        public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<Publication> Publications { get; set; }
+        public DbSet<Volume> Volumes { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Book>().Property(b => b.ISBN).HasMaxLength(13);
+            modelBuilder.Entity<Journal>().Property(b => b.ISSN).HasMaxLength(13);
+            modelBuilder.Entity<Article>().Property(a => a.Title).IsRequired();
+            modelBuilder.Entity<Article>().Property(a => a.Abstract).IsRequired();
+            modelBuilder.Entity<Author>().Property(a => a.FullName).IsRequired();
+            modelBuilder.Entity<Author>().Property(a => a.Email).IsRequired();
+            modelBuilder.Entity<Book>().Property(b => b.Description).IsRequired();
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired();
+            modelBuilder.Entity<Book>().Property(b => b.ISBN).IsRequired();
+            modelBuilder.Entity<Journal>().Property(j => j.ISSN).IsRequired();
+            modelBuilder.Entity<Journal>().Property(j => j.Name).IsRequired();
+            modelBuilder.Entity<Keyword>().Property(k => k.Word).IsRequired();
+            modelBuilder.Entity<Publication>().Property(p => p.Name).IsRequired();
 
         }
     }
---
+++ Core/Migrations/202212271421533_AddedMaxLenghts.cs
@@ -0,0 +1,44 @@
+﻿namespace Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedMaxLenghts : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.ArticleAuthors", newName: "AuthorArticles");
+            DropPrimaryKey("dbo.AuthorArticles");
+            AlterColumn("dbo.Authors", "FullName", c => c.String(nullable: false));
+            AlterColumn("dbo.Authors", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Articles", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Articles", "Abstract", c => c.String(nullable: false));
+            AlterColumn("dbo.Keywords", "Word", c => c.String(nullable: false));
+            AlterColumn("dbo.Journals", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Journals", "ISSN", c => c.String(nullable: false, maxLength: 13));
+            AlterColumn("dbo.Publications", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Books", "ISBN", c => c.String(nullable: false, maxLength: 13));
+            AlterColumn("dbo.Books", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Books", "Description", c => c.String(nullable: false));
+            AddPrimaryKey("dbo.AuthorArticles", new[] { "Author_Id", "Article_Id" });
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.AuthorArticles");
+            AlterColumn("dbo.Books", "Description", c => c.String());
+            AlterColumn("dbo.Books", "Title", c => c.String());
+            AlterColumn("dbo.Books", "ISBN", c => c.String());
+            AlterColumn("dbo.Publications", "Name", c => c.String());
+            AlterColumn("dbo.Journals", "ISSN", c => c.String());
+            AlterColumn("dbo.Journals", "Name", c => c.String());
+            AlterColumn("dbo.Keywords", "Word", c => c.String());
+            AlterColumn("dbo.Articles", "Abstract", c => c.String());
+            AlterColumn("dbo.Articles", "Title", c => c.String());
+            AlterColumn("dbo.Authors", "Email", c => c.String());
+            AlterColumn("dbo.Authors", "FullName", c => c.String());
+            AddPrimaryKey("dbo.AuthorArticles", new[] { "Article_Id", "Author_Id" });
+            RenameTable(name: "dbo.AuthorArticles", newName: "ArticleAuthors");
+        }
+    }
+}
