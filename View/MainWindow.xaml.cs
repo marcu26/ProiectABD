8852dddc5e9b6386d64617e783fa548f32555663
pascu andrei <pascu.andrei@isoft-development.ro>
2022-12-30 16:51:52 +0200

    whole UI change and backend integration

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -46,6 +46,9 @@ namespace View
 
         public static BooksRepository _booksRepository = new BooksRepository(_dbContext);
 
+        public static KeywordsRepository _keywordsRepository = new KeywordsRepository(_dbContext);
+
+
         public List<string> PublicationTypes =  new List<string>() {"Article", "Book", "Journal" , "Publication", "Volume"};
 
         #endregion
@@ -53,66 +56,150 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
-            TypeFilter.ItemsSource = PublicationTypes;
+            sBar1TextBlock.Text = "Search here...";
+            sGrid2.Visibility = Visibility.Hidden;
+            sGrid3.Visibility = Visibility.Hidden;
+            sGrid4.Visibility = Visibility.Hidden;
+            YearStackPanel.Visibility = Visibility.Hidden;
+            AuthorsComboBox.Visibility = Visibility.Hidden;
+            KeywordsComboBox.Visibility = Visibility.Hidden;
         }
-        private async void TypeFilter_SelectedIndexChanged(object sender, RoutedEventArgs e)
-        {
-            string current_selection = TypeFilter.SelectedItem.ToString();
 
-            if(current_selection == "Article" )
+
+        private bool IsMaximize = false;
+
+        private static string LastTypeSelected;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
             {
-                FieldsFilter.ItemsSource = typeof(Article).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
-                //List<ArticlesDto> articles = await _articlesRepository.GetArticlesDtoByVolumeId(672);
-                List<ArticlesDto> articles = await _articlesRepository.GetArticlesDtoByFiltersAsync(null, null, new List<string>() { "Aisha Levine", "Kristen Fletcher" }, null);
-                ListView.ItemsSource = articles;
-            }
-            if(current_selection == "Book" )
-            {
-                FieldsFilter.ItemsSource = typeof(Book).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
-                List<BooksDto> books = await _booksRepository.GetBooksDtoByFilters(null, null, null, null);
-                ListView.ItemsSource = books;
-            }
-            if(current_selection == "Journal" )
-            {
-                FieldsFilter.ItemsSource = typeof(Journal).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
-                List<JournalsDto> journals = await _journalsRepository.GetJournalsDtoByFiltersAsync("z","7");
-                //List<JournalsDto> journals = await _journalsRepository.GetJournalsDtoByPublicationIdAsync(34);
-                ListView.ItemsSource = journals;
-            }
-            if(current_selection == "Publication")
-            {
-                FieldsFilter.ItemsSource = typeof(Publication).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
-                List<PublicationsDto> publications =await _publicationsRepository.GetPublicationsDtoByFiltersAsync("e", 2006, 2009);
-                ListView.ItemsSource = publications;
-            }
-            if(current_selection == "Volume")
-            {
-                FieldsFilter.ItemsSource = typeof(Volume).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
-                // List<VolumesDto> volumes = await _volumesRepository.GetVolumesDtoByFiltersAsync(0, 2000, 2010);
-                List<VolumesDto> volumes = await _volumesRepository.GetVolumesDtoByJournalId(547);
-                ListView.ItemsSource = volumes;
+                if (IsMaximize)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximize = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximize = true;
+                }
             }
         }
 
-        private void SearchBar_GotFocus(object sender, RoutedEventArgs e)
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            SearchBar.Text = string.Empty;
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
 
-        private void SearchBar_LostFocus(object sender, RoutedEventArgs e)
+        private async void searchButton_Click(object sender, RoutedEventArgs e)
         {
-            SearchBar.Text = "Enter text...";
+
+            List<string> selectedAuthors = AuthorsComboBox.SelectedItems.Cast<string>().ToList();
+            if(selectedAuthors.Count() == 0)
+            {
+                selectedAuthors = null;
+            }
+
+            List<string> selectedKeywords = KeywordsComboBox.SelectedItems.Cast<string>().ToList();
+            if(selectedKeywords.Count() == 0)
+            {
+                selectedKeywords = null;
+            }
+
+            string titleFilter = searchBar1.Text;
+            if (String.IsNullOrEmpty(titleFilter))
+            {
+                titleFilter = null;
+            }
+
+            string abstractFilter = searchBar2.Text;
+            if (String.IsNullOrEmpty(abstractFilter))
+            {
+                abstractFilter = null;
+            }
+
+            List<ArticlesDto> articles = await _articlesRepository.GetArticlesDtoByFiltersAsync(titleFilter, abstractFilter, selectedAuthors, selectedKeywords);
+            DataGrid.AutoGenerateColumns = true;
+            DataGrid.ItemsSource = articles;
         }
 
-        private void KeywordFilter_GotFocus(object sender, RoutedEventArgs e)
+        private async void ArticlesButton_Click(object sender, RoutedEventArgs e)
         {
-            KeywordFilter.Text = string.Empty;
-        }
+            LastTypeSelected = "Article";
+            sBar1TextBlock.Text = "Title";
+            sBar2TextBlock.Text = "Description";
+            sGrid2.Visibility = Visibility.Visible;
+            sGrid3.Visibility = Visibility.Hidden;
+            sGrid4.Visibility = Visibility.Hidden;
+            YearStackPanel.Visibility = Visibility.Hidden;
+            AuthorsComboBox.Visibility = Visibility.Visible;
+            KeywordsComboBox.Visibility = Visibility.Visible;
 
-        private void KeywordFilter_LostFocus(object sender, RoutedEventArgs e)
-        {
-            KeywordFilter.Text = "Enter a keyword...";
+            List<string> authors = await _authorsRepository.GetAuthorsAsync();
+            AuthorsComboBox.ItemsSource = authors;
+
+            List<string> keywords = await _keywordsRepository.GetKeywordsAsync();
+            KeywordsComboBox.ItemsSource = keywords;
+
+
+            
         }
+        //private async void TypeFilter_SelectedIndexChanged(object sender, RoutedEventArgs e)
+        //{
+        //    string current_selection = TypeFilter.SelectedItem.ToString();
+
+        //    if(current_selection == "Article" )
+        //    {
+        //        FieldsFilter.ItemsSource = typeof(Article).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
+        //        //List<ArticlesDto> articles = await _articlesRepository.GetArticlesDtoByVolumeId(672);
+        //        List<ArticlesDto> articles = await _articlesRepository.GetArticlesDtoByFiltersAsync(null, null, new List<string>() { "Aisha Levine", "Kristen Fletcher" }, null);
+        //        ListView.ItemsSource = articles;
+        //    }
+        //    if(current_selection == "Book" )
+        //    {
+        //        FieldsFilter.ItemsSource = typeof(Book).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
+        //        List<BooksDto> books = await _booksRepository.GetBooksDtoByFilters(null, null, null, null);
+        //        ListView.ItemsSource = books;
+        //    }
+        //    if(current_selection == "Journal" )
+        //    {
+        //        FieldsFilter.ItemsSource = typeof(Journal).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
+        //        List<JournalsDto> journals = await _journalsRepository.GetJournalsDtoByFiltersAsync("z","7");
+        //        //List<JournalsDto> journals = await _journalsRepository.GetJournalsDtoByPublicationIdAsync(34);
+        //        ListView.ItemsSource = journals;
+        //    }
+        //    if(current_selection == "Publication")
+        //    {
+        //        FieldsFilter.ItemsSource = typeof(Publication).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
+        //        List<PublicationsDto> publications =await _publicationsRepository.GetPublicationsDtoByFiltersAsync("e", 2006, 2009);
+        //        ListView.ItemsSource = publications;
+        //    }
+        //    if(current_selection == "Volume")
+        //    {
+        //        FieldsFilter.ItemsSource = typeof(Volume).GetProperties().Select(p => p.Name).Where(n => n != "Id" && n != "IsDeleted");
+        //        // List<VolumesDto> volumes = await _volumesRepository.GetVolumesDtoByFiltersAsync(0, 2000, 2010);
+        //        List<VolumesDto> volumes = await _volumesRepository.GetVolumesDtoByJournalId(547);
+        //        ListView.ItemsSource = volumes;
+        //    }
+        //}
+
+        //private void SearchBar_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    SearchBar.Text = string.Empty;
+        //}
+
+        //private void SearchBar_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    SearchBar.Text = "Search here...";
+        //}
+
 
 
 
